0d94aeb68de835d44b90fcdb01e3bbcbfae3805b
Ian Pedro <ian0198@gmail.com>
2017-08-18 00:40:01 -0300

    Feat: Working on multiple methods

--- Lista10/questao1.c
+++ Lista10/questao1.c
@@ -1,53 +1,91 @@
 #include <stdio.h>
+
 int somar(int);
+
 int isPair(int);
-int calculaMedia(int);
+
+float calculaMedia(float);
+
 float dobrar(float);
 
+int checaMaior(int);
+
+int checaMenor(int);
+
+int numMaior(int);
+
+int calculaFatorial(int);
+
+int calculaProduto(int);
+
+
 int main() {
-    int opInicial;
-    while(opInicial != 0){
+    int opInicial, opFuncao;
+    while (opInicial != 0) {
         printf("Digite 1 para ver o menu ou 0 para finalizar o programa");
-        scanf("%d",&opInicial);
+        scanf("%d", &opInicial);
+        printf("Digite a opção desejada: ");
+        scanf("%d", &opFuncao);
+        switch (opFuncao) {
+            case (1):
+                int n1;
+                int n2;
+                int soma;
+                printf("Digite os numeros a serem somados:\n");
+                scanf("%d%d", &n1, &n2);
+                soma = somar(&n1, &n2);
+                printf("Soma de %d e %d é igual a: %d", n1, n2, soma);
+                break;
+            case (2):
+                int n1;
+                printf("Digite o numero a ser checado:\n");
+                scanf("%d", &n1);
+                if (isPair(&n1)==1)
+                    printf("O número digitado é par");
+                else
+                    printf("O número digitado é impar");
+            default:
+                printf("Opcao incorreta!");
+        }
     }
 }
 
-int somar(int *a, int *b){
-    return *a+*b;
+int somar(int *a, int *b) {
+    return *a + *b;
 }
 
-int isPair(int *a){
+int isPair(int *a) {
     if (*a % 2 == 0)
         return 1;
     return 0;
 
 }
 
-float calculaMedia(float *a,float *b){
-    return (*a+*b)/2;
+float calculaMedia(float *a, float *b) {
+    return (*a + *b) / 2;
 
 }
 
-float dobrar(float *a){
-    return (*a)*2;
+float dobrar(float *a) {
+    return (*a) * 2;
 
 }
 
-int checaMaior(int *x,int *y){
+int checaMaior(int *x, int *y) {
     if (*x > *y)
         return *x;
     return *y;
 
 }
 
-int checaMenor(int *x,int *y){
+int checaMenor(int *x, int *y) {
     if (*x < *y)
         return *x;
     return *y;
 
 }
 
-int numMaior (int a, int b, int c){
+int numMaior(int a, int b, int c) {
     int m;
     if (a > b && a > c)
         m = a;
@@ -58,16 +96,16 @@ int numMaior (int a, int b, int c){
     return m;
 }
 
-int somatorio (int num){
+int calculaFatorial(int num) {
     int s;
-    for(s = 1; num > 1; num = num - 1)
+    for (s = 1; num > 1; num = num - 1)
         s = s + num;
     return s;
 }
 
-int produto (int num){
+int calculaProduto(int num) {
     int p;
-    for(p = 1; p == num; p++)
+    for (p = 1; p == num; p++)
         p = p * (p + 1);
     return p;
 }
